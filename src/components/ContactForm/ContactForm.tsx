9f92bc334277cd416175a06ff0f6ca7faace78b2
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-03-10 22:47:48 +0200

    v.1.2. Added components ContactForm and ContactUs

---
+++ src/components/ContactForm/ContactForm.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { IContactFormValue } from "../../types/IContactFormValue";
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
+import { contactFormSchema } from "../../helpers";
+import cn from "classnames";
+import styles from "./ContactForm.module.scss";
+
+const ContactForm: React.FC = () => {
+  const [isMinted, setIsMinted] = useState(false);
+
+  const initialValues: IContactFormValue = {
+    discord: "",
+    metamask: "",
+  };
+
+  const onSubmit = (
+    value: IContactFormValue,
+    { resetForm }: FormikHelpers<IContactFormValue>
+  ) => {
+    console.log("Discord: ", value.discord.toUpperCase());
+    console.log("MetaMask: ", value.metamask.toUpperCase());
+    setIsMinted(true);
+
+    setTimeout(() => {
+      setIsMinted(false);
+      resetForm();
+    }, 4000);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={contactFormSchema}
+      onSubmit={onSubmit}
+    >
+      {({ errors: { discord, metamask }, touched }) => {
+        const buttonText =
+          discord || metamask ? "ERROR" : isMinted ? "MINTED" : "MINT";
+        return (
+          <Form className={styles.form}>
+            <div className={styles.form__input_wrapper}>
+              <div className={styles.form__icon_container}></div>
+
+              <div className={styles.form__field}>
+                <Field
+                  id="discord"
+                  name="discord"
+                  type="text"
+                  className={cn(styles.form__input, {
+                    [styles.error]: touched.discord && discord,
+                  })}
+                  autoComplete="off"
+                  placeholder="@username"
+                />
+                <ErrorMessage
+                  name="discord"
+                  component="p"
+                  className={styles.form__error}
+                />
+              </div>
+            </div>
+
+            <div className={styles.form__input_wrapper}>
+              <div className={styles.form__icon_container}></div>
+
+              <div className={styles.form__field}>
+                <Field
+                  id="metamask"
+                  name="metamask"
+                  type="text"
+                  className={cn(styles.form__input, {
+                    [styles.error]: touched.metamask && metamask,
+                  })}
+                  autoComplete="off"
+                  placeholder="Wallet address"
+                />
+                <ErrorMessage
+                  name="metamask"
+                  component="p"
+                  className={styles.form__error}
+                />
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              aria-label="Submit MINT form"
+              className={styles.form__submit}
+            >
+              {buttonText}
+            </button>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default ContactForm;
---
+++ src/components/ContactUs/ContactUs.tsx
@@ -0,0 +1,19 @@
+import ContactForm from "../ContactForm/ContactForm";
+import styles from "./ContactUs.module.scss";
+
+const ContactUs: React.FC = () => {
+  return (
+    <section className={styles.contact} id="mint">
+      <h2 className={styles.contact_title}>Are you in?</h2>
+
+      <div className={styles.contact__text_container}>
+        <p className={styles.contact__text}>
+          Join the YACHT APE community to be one of the first to receive our
+          limited edition NFT
+        </p>
+      </div>
+      <ContactForm />
+    </section>
+  );
+};
+export default ContactUs;
