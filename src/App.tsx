250afc017a3f050436886e6aa4bd593ca38b475c
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-03-13 22:06:32 +0200

    v.1.7. Added component  About + App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import Header from "./components/Header";
+import FAQ from "./components/FAQ";
+import ContactUs from "./components/ContactUs";
+import { useEffect, useRef, useState } from "react";
+import About from "./components/About";
 
 const App: React.FC = () => {
-  return <></>;
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (aboutRef.current) {
+        const aboutTop = aboutRef.current.getBoundingClientRect().top;
+        setIsScrolled(aboutTop <= 0);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <Header isScrolled={isScrolled}/>
+      <main>
+        <About innerRef={aboutRef} />
+        <FAQ />
+        <ContactUs />
+      </main>
+    </>
+  );
 };
 
 export default App;
