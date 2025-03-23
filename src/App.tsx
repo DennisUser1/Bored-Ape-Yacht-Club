1d788abf2fbbfd7a793c3e6216e5bd83b3b1f5be
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-03-23 09:55:15 +0200

    v.1.31.Fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <>
+    <div className={"box"}>
       <Header isScrolled={isScrolled}/>
       <main>
         <About innerRef={aboutRef} />
@@ -38,7 +38,7 @@ const App: React.FC = () => {
         <ContactUs />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
